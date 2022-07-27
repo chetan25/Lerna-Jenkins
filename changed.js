23687d87d4dc844d1c5611f254109841589ae490
Chetan25 <chetandasauni25@gmail.com>
2022-07-26 20:59:58 -0700

    updated changed file

--- changed.js
+++ changed.js
@@ -6,31 +6,32 @@ const shell = require("shelljs");
 
 function getChanged() {
   try {
-    GIT_LATEST_TAG_COMMAND = "git describe origin/main";
-    const { stdout: GIT_LATEST_TAG } = shell.exec(GIT_LATEST_TAG_COMMAND, {
-      silent: true,
-    });
+    // GIT_LATEST_TAG_COMMAND = "git describe origin/main";
+    // const { stdout: GIT_LATEST_TAG } = shell.exec(GIT_LATEST_TAG_COMMAND, {
+    //   silent: true,
+    // });
     // console.log(GIT_LATEST_TAG.trim(), "GIT_LATEST_TAG");
 
-    GIT_BRANCH_COMMAND = "git rev-parse --abbrev-ref HEAD";
-    const { stdout: GIT_BRANCH } = shell.exec(GIT_BRANCH_COMMAND, {
-      silent: true,
-    });
-    console.log("GIT_BRANCH", GIT_BRANCH.trim());
+    // GIT_BRANCH_COMMAND = "git rev-parse --abbrev-ref HEAD";
+    // const { stdout: GIT_BRANCH } = shell.exec(GIT_BRANCH_COMMAND, {
+    //   silent: true,
+    // });
+    // console.log("GIT_BRANCH", GIT_BRANCH.trim());
 
-    const COMMIT_HASH = `git merge-base ${GIT_BRANCH.trim()}  ${GIT_LATEST_TAG.trim()}`;
-    console.log(COMMIT_HASH);
-    const { stdout: LAST_HASH } = shell.exec(COMMIT_HASH, {
-      silent: true,
-    });
-    console.log("LAST_HASH", LAST_HASH);
+    // const COMMIT_HASH = `git merge-base ${GIT_BRANCH.trim()}  ${GIT_LATEST_TAG.trim()}`;
+    // console.log(COMMIT_HASH);
+    // const { stdout: LAST_HASH } = shell.exec(COMMIT_HASH, {
+    //   silent: true,
+    // });
+    // console.log("LAST_HASH", LAST_HASH);
 
     // const changedPackages = `npx lerna ls -p --since ${LAST_HASH.trim()} --include-dependents`;
 
-    const changedPackages = `npx lerna ls -p -- since HEAD~1 --include-dependents`;
+    // const changedPackages = `npx lerna ls -p -- since HEAD~1 --include-dependents`;
+    const changedPackages = `npx lerna changed --json`;
     // const changed = execSync(changedPackages).toString();
     const { stdout: changed } = shell.exec(changedPackages);
-    console.log("changed", changed);
+    // console.log("changed", changed);
     return changed;
   } catch (e) {
     console.log(e);
@@ -38,16 +39,17 @@ function getChanged() {
 }
 
 const main = () => {
-  const changedPackages = getChanged();
-  const packagesPath = changedPackages.split("\n");
-  console.log(changedPackages.split("\n"));
+  const changedPackages = JSON.parse(getChanged());
+  // const packagesPath = changedPackages.split("\n");
+  console.log(typeof changedPackages);
 
   // will be used by lerna to publish packages
   const packagesToUpdate = [];
-  packagesPath.forEach((package) => {
+  let canPublish = true;
+  changedPackages.forEach((package) => {
     if (package) {
-      shell.cd(`${package}`);
-      shell.echo("Running scripts for " + package);
+      shell.cd(`${package.location}`);
+      shell.echo("Running scripts for " + package.name);
       shell.exec("pwd");
       shell.exec("npm ci");
       const { stdout, stderr, code } = shell.exec("npm run check");
@@ -55,12 +57,19 @@ const main = () => {
       console.log(stdout, "stdout");
       if (code < 1) {
         packagesToUpdate.push(package);
+        canPublish = false;
       }
     }
   });
   console.log(packagesToUpdate);
+  // if (packagesToUpdate.length )
   //  npx lerna publish --skip-npm --yes minor
+
+  // won't work as we cannot publish individual packages
   // npx lerna version --ignore-changes 'D:\Programming\git-jenkins\lerna-jenkins\packages\appB'
+
+  // to run command in selected packages in parallel
+  // npx lerna exec --parallel --scope '{appa,appb}' -- npm run check
 };
 
 main();
